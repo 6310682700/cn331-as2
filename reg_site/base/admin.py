e84d61e6eb68ba1931451b6b526db617291f1565
6310682700 <69451450+6310682700@users.noreply.github.com>
2022-09-10 19:04:15 +0700

    add commit page

--- reg_site/base/admin.py
+++ reg_site/base/admin.py
@@ -1,3 +1,10 @@
+from calendar import c
+from turtle import st
 from django.contrib import admin
+from .models import Student, Class, Enroll
 
 # Register your models here.
+
+admin.site.register(Student)
+admin.site.register(Class)
+admin.site.register(Enroll)
--- reg_site/base/models.py
+++ reg_site/base/models.py
@@ -7,18 +7,22 @@ from django.contrib.auth.models import User
 class Student(models.Model):
     StudentUser = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
     StudentCode = models.CharField(max_length=10, null=False, blank=False)
-    StudentName = models.CharField(max_length=100, null=False, blank=False)
     StudentYear = models.IntegerField(null=True, blank=True)
     StudentSem = models.IntegerField(null=True, blank=True)
 
+    def __str__(self):
+        return self.StudentUser.first_name
+
 class Class(models.Model):
-    ClassCode = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    ClassCode = models.CharField(max_length=10, primary_key=True)
     ClassName = models.CharField(max_length=100, null=False, blank=False)
-    ClassYear = models.CharField(max_length=100, null=False, blank=False)
-    ClassSem = models.CharField(max_length=100, null=False, blank=False)
+    ClassYear = models.CharField(max_length=100, null=True, blank=True)
+    ClassSem = models.CharField(max_length=100, null=True, blank=True)
     ClassCapacity = models.IntegerField(null=False, blank=False)
+
+    def __str__(self):
+        return self.ClassCode
 
 class Enroll(models.Model):
     EnrollUser = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
     EnrollClass = models.ForeignKey(Class, on_delete=models.CASCADE, null=False, blank=False)
-    Enrolled = models.BooleanField(null=False, blank=False)
