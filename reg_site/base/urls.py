6cbd0d0657fa6cbf96fbc442b9c6909d52bb27fe
6310682700 <69451450+6310682700@users.noreply.github.com>
2022-09-13 20:18:41 +0700

    add new login logut

--- reg_site/base/urls.py
+++ reg_site/base/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.taskList, name='tasks'),
+    path('', views.index, name='index'),
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
 ]
--- reg_site/reg_site/urls.py
+++ reg_site/reg_site/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from pipes import Template
 from django.contrib import admin
 from django.urls import path, include
+from base import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("account/", include('django.contrib.auth.urls')),
+    path("users/", include('base.urls')),
 ]
