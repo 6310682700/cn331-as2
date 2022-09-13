eb1042dfb4a4237b092fe02a1aa3a008efff7d11
6310682692 <6310682692@student.tu.ac.th>
2022-09-13 22:44:32 +0700

    make change database

--- reg_site/base/admin.py
+++ reg_site/base/admin.py
@@ -1,10 +1,9 @@
 from calendar import c
 from turtle import st
 from django.contrib import admin
-from .models import Student, Class, Enroll
+from .models import Student, Class
 
 # Register your models here.
 
 admin.site.register(Student)
 admin.site.register(Class)
-admin.site.register(Enroll)
--- reg_site/base/models.py
+++ reg_site/base/models.py
@@ -5,24 +5,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Student(models.Model):
-    StudentUser = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
-    StudentCode = models.CharField(max_length=10, null=False, blank=False)
-    StudentYear = models.IntegerField(null=True, blank=True)
-    StudentSem = models.IntegerField(null=True, blank=True)
+    firstname = models.CharField(max_length=99)
+    surname = models.CharField(max_length=99)
+    studentid = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.StudentUser.first_name
+        return f"({self.name} ({self.surname} {self.studentid})"
 
 class Class(models.Model):
-    ClassCode = models.CharField(max_length=10, primary_key=True)
-    ClassName = models.CharField(max_length=100, null=False, blank=False)
-    ClassYear = models.CharField(max_length=100, null=True, blank=True)
-    ClassSem = models.CharField(max_length=100, null=True, blank=True)
-    ClassCapacity = models.IntegerField(null=False, blank=False)
+    subject = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="Subject")
+    capacity = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="capacity")
 
     def __str__(self):
-        return self.ClassCode
-
-class Enroll(models.Model):
-    EnrollUser = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
-    EnrollClass = models.ForeignKey(Class, on_delete=models.CASCADE, null=False, blank=False)
+        return f"{self.subject} ({self.capacity})"
