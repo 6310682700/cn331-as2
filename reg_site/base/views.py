4d51c418ad078bb6045a246a61884724ed4b511e
6310682700 <69451450+6310682700@users.noreply.github.com>
2022-09-16 22:12:43 +0700

    change

--- reg_site/base/views.py
+++ reg_site/base/views.py
@@ -2,12 +2,7 @@ from audioop import reverse
 from cgitb import html
 from email import message
 from http.client import HTTPResponse
-<<<<<<< HEAD
 from re import sub
-=======
-import imp
-from unicodedata import name
->>>>>>> 805505f26bebf9e4cf09e4cc3710865e50dbb88d
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
@@ -24,16 +19,10 @@ from .models import Course, Student, Enroll
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login'))
-<<<<<<< HEAD
     student = Student.objects.get(Student_Users_id=request.user.id)
     return render(request, 'users/index.html', {
         "courses": student.courses.all()
     })
-=======
-    return render(request, 'users/index.html',{
-        "Class" : Class.objects.all() 
-        })
->>>>>>> 805505f26bebf9e4cf09e4cc3710865e50dbb88d
     
 
 def login_view(request):
@@ -56,7 +45,6 @@ def logout_view(request):
         'message': 'You have logged out.'
     })
 
-<<<<<<< HEAD
 
 def enroll(request):
     if not request.user.is_authenticated:
@@ -75,22 +63,3 @@ def enroll(request, coures):
         student = Student.objects.get(Student_Users_id=request.user.id)
         student.course.add(course)
         return HttpResponseRedirect("index.html")
-=======
-def regist(request):
-    if request.method == "POST":
-        Enroll = Student.enroll.add(Class.Subject)
-        Class.capacity = Class.capacity + 1
-        return render(request, 'index.html', {
-            "Enrollment" : Enroll
-    })
-
-def remove(request):
-    if request.method == "POST":
-        Enroll = Student.enroll.remove(Class.Subject)
-        Class.capacity = Class.capacity - 1
-        return render(request, 'index.html', {
-            "Enrollemnt" : Enroll
-    })
-
-
->>>>>>> 805505f26bebf9e4cf09e4cc3710865e50dbb88d
