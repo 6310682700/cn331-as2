0aab5319f14ed03250ed280a2cf345a4523d98f0
6310682692 <6310682692@student.tu.ac.th>
2022-10-05 20:39:18 +0700

    test(undone)

--- reg_site/base/tests.py
+++ reg_site/base/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.test import TestCase, client
+from .models import Course, Student , Enroll
+from django.urls import reverse 
+from django.db.models import Max
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class testModel(TestCase):
+
+    def setUp(self):
+        user = User.objects.create("non")
+        course1 = Course.objects.create(Subject="AA",Subject_name = "A", Year = 1 ,Semester = 1, capacity = 1, status = True )
+        course2 = Course.objects.create(Subject="BB",Subject_name = "B", Year = 1 ,Semester = 1, capacity = 1, status = True )
+
+        student = Student.objects.create(
+            Student_Users = user
+        )
+
+        enroll = Enroll.objects.create(
+            Student = user , course= "course1"
+        )
+
+    def test_index_status(self):
+        c = client()
+        response = c.get(reverse('users:index'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_registration(self):
+        student = Student.objects.create("non")
+        s = User.objects.first()
+        s.capacity = 1
+        s.save()
+
+        c = client()
+        response = c.get(reverse('users:enroll'))
+        self.assertEqual(response.context['users'].count(), 1)
+
+    def test_regist_page(self):
+        c = client()
+        response = c.get(reverse('users:enroll'))
+        self.assertEqual(response.status_code, 200)
--- reg_site/reg_site/settings.py
+++ reg_site/reg_site/settings.py
@@ -122,10 +122,10 @@ STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static'),
 )
 
-import django_heroku
-django_heroku.settings(locals())
+#import django_heroku
+#django_heroku.settings(locals())
 
 
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+#import dj_database_url 
+#prod_db  =  dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(prod_db)
